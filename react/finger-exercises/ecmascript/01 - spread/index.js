67d6559223b22f09ac9ec6f0dfbba706627e6fc0
Ivo Biaus <ivo.biaus@wolox.com.ar>
2019-06-05 10:44:17 -0300

    spread-test-03

--- react/finger-exercises/ecmascript/01 - spread/index.js
+++ react/finger-exercises/ecmascript/01 - spread/index.js
@@ -1,8 +1,14 @@
 import { isArray } from './utils';
 
-export function min(numbers) {
+export function min() {
+  var numbers = arguments[0];
+  var args = Array.from(arguments);
+  
   if(isArray(numbers)){
     return Math.min.apply(Math, numbers);
+  }
+  else if (isArray(args)){
+    return Math.min.apply(Math, args);
   }
   return numbers;
 

7182c7d913acf3e643412be196838b21ba0689d6
Ivo Biaus <ivo.biaus@wolox.com.ar>
2019-06-12 11:17:52 -0300

    modified actions and reducers in order to pass the tests

--- react/finger-exercises/redux/01 - actions and reducers/auth/reducer.js
+++ react/finger-exercises/redux/01 - actions and reducers/auth/reducer.js
@@ -1,10 +1,22 @@
+import { actions } from './actions';
+
 const initialState = {
   email: null,
   token: null
 };
 
-function reducer(state = initialState) {
-  return state;
+function reducer(state = initialState, action) {
+  switch(action.type){
+    case actions.LOG_IN:
+      return {
+        ...state,
+        email: action.payload.credentials.email,
+        token: action.payload.credentials.token
+      };
+
+    default:
+        return state;
+  }
 }
 
 export default reducer;
--- react/finger-exercises/redux/01 - actions and reducers/books/reducer.js
+++ react/finger-exercises/redux/01 - actions and reducers/books/reducer.js
@@ -11,7 +11,8 @@ don't add it to the initialState, they'll probably won't notice it's there.
 */
 const initialState = {
   favoriteBook: null,
-  readBooks: ['Moby Dick']
+  readBooks: ['Moby Dick'],
+  mostHatedBook: null
 };
 
 /*
@@ -26,6 +27,17 @@ function reducer(state = initialState, action) {
         ...state,
         favoriteBook: action.payload.favoriteBook
       };
+
+    case actions.ADD_READ_BOOK:
+      state.readBooks.push(action.payload.title);
+      return state;
+
+    case actions.SET_MOST_HATED_BOOK:
+      return {
+        ...state,
+        mostHatedBook: action.payload.mostHatedBook
+      };
+
     default:
       return state;
   }

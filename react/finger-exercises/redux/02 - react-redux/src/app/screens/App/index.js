819e548cab1579fb6c3a7d248b3492d04e24da0c
Ivo Biaus <ivo.biaus@wolox.com.ar>
2019-06-13 09:36:48 -0300

    implemented the logic of the reducer and implemented the dispatchers at the main index

--- react/finger-exercises/redux/02 - react-redux/src/app/screens/App/index.js
+++ react/finger-exercises/redux/02 - react-redux/src/app/screens/App/index.js
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from 'react';
 import store from '@redux/store';
 import Navbar from '@components/Navbar';
 import Footer from '@components/Footer';
+import actions from '@redux/book/actions';
 
 import Book from './components/Book';
 import Search from './components/Search';
@@ -20,19 +21,28 @@ class App extends Component {
       this.setState({ books, bookSelected });
     });
     // TODO to implement the dispatch
+    store.dispatch(actions.getBooks());
   }
 
   // TODO to implement the dispatch
-  onSearch = value => {};
+  onSearch = value => {
+    store.dispatch(actions.searchBook(value));
+  };
 
   // TODO to implement the dispatch
-  addToCart = item => {};
+  addToCart = item => {
+    store.dispatch(actions.addToCart(item));
+  };
 
   // TODO to implement the dispatch
-  addItem = itemId => {};
+  addItem = itemId => {
+    store.dispatch(actions.addItem(itemId));
+  };
 
   // TODO to implement the dispatch
-  removeItem = itemId => {};
+  removeItem = itemId => {
+    store.dispatch(actions.removeItem(itemId));
+  };
 
   CONFIGURATION_BUTTON = {
     add: {

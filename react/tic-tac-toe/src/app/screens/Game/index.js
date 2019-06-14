1d5c72ec351a55195f83bf8368e8403f3c424dfa
Ivo Biaus <ivo.biaus@wolox.com.ar>
2019-06-14 13:34:44 -0300

    showing the list of matches obteined from the tic-tac-toe API

--- react/tic-tac-toe/src/app/screens/Game/index.js
+++ react/tic-tac-toe/src/app/screens/Game/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import matchesService from './../../../services/MatchesService';
 
 import styles from './styles.module.scss';
 import Board from './components/Board';
@@ -8,11 +8,13 @@ class Game extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      matches: [],
       history: [{
         squares: Array(9).fill(null),
       }],
       stepNumber: 0,
       xIsNext: true,
+      isLoading: false,
     };
   }
 
@@ -36,6 +38,15 @@ class Game extends Component {
       stepNumber: history.length,
       xIsNext: !this.state.xIsNext,
     });
+  }
+
+  async componentDidMount(){
+    this.state.isLoading = true;
+    const response = await matchesService.getMatches();
+    if(response.ok){
+      this.state.matches = response.data;
+      this.state.isLoading = false;
+    }
   }
 
   getMovesList(){
@@ -68,20 +79,37 @@ class Game extends Component {
     const moves = this.getMovesList();
 
 
-    return (
-      <div className={styles.game}>
-        <div className={styles.gameBoard}>
-          <Board 
-            squares={current.squares}
-            onClick={(i) => this.handleClick(i)}
-          />
+    const matchesHistory = this.state.matches;
+    const matches = matchesHistory.map(game => {
+      const match = "Player 1: " + game.player_one + " || Player 2: " +  game.player_two + " || Winner: " + game.winner;
+      return (
+        <li key={game.id} >
+            {match}
+        </li>
+      );
+    });
+    var Spinner = require('react-spinkit');
+
+    if(this.state.isLoading){
+      return (<Spinner name='folding-cube' />);
+    }else{
+      return (
+        <div className={styles.game}>  
+          <div className={styles.gameBoard}>
+            <Board 
+              squares={current.squares}
+              onClick={(i) => this.handleClick(i)}
+            />
+          </div>
+          <div className={styles.gameInfo}>
+            <span>{status}</span>
+            <ol>{moves}</ol>
+          </div>
+          <ol>{matches}</ol>
         </div>
-        <div className={styles.gameInfo}>
-          <span>{status}</span>
-          <ol>{moves}</ol>
-        </div>
-      </div>
-    );
+      );
+    }
+
   }
 
   calculateWinner(squares) {
